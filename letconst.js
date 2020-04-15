fbd13d28b5aaf11686a9fe646cb98434f71b8f09
mamun1336 <abmamun504@gmail.com>
2020-04-16 04:54:20 +0600

    let and constant

---
+++ letconst.js
@@ -0,0 +1,29 @@
+const capital = "Dhaka";
+// capital = "rampura";
+console.log(capital);
+
+const numbers = [12, 45, 33];
+console.log(numbers);
+numbers[1] = 88;
+console.log(numbers);
+numbers.push(45);
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+
+// numbers = ["mamun", "apu", "abdullah"];
+console.log(numbers);
+
+const nayok = {name:"sakib khan", phone: 4567};
+console.log(nayok);
+
+let patientName = "Rahim chacha";
+console.log(patientName);
+patientName = "Rahima khala";
+console.log(patientName);
+let sum = 0;
+for(let i = 0; i < 10;i++){
+    sum = sum+i;
+    console.log(sum);
+}
+// console.log(i);
