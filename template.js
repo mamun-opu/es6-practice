d472c2f35b9cdfbbb47a88ff36352f5f0f50498b
mamun1336 <abmamun504@gmail.com>
2020-04-18 03:39:22 +0600

    template

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Mamun";
+const LastName = "Apu";
+const fullName = firstName + " " + LastName +" is a good boy!";
+const fullName2 = `${firstName} ${LastName} is a good boy!`;
+console.log(fullName);
+console.log(fullName2);
+const multiLine = "He LOves to listen song!\n"+"Playing guiter!";
+console.log(multiLine);
+const multiLine2 = `He LOves to play football,
+playing cricket!`
+console.log(multiLine2);
