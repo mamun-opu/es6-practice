8a219ad1a78eb3738e966fd670786bcb68507bf9
mamun1336 <abmamun504@gmail.com>
2020-04-19 00:04:37 +0600

    inheritance

---
+++ class.js
@@ -0,0 +1,10 @@
+class student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Amirabari School"
+    }
+}
+const student1 = new student(12,"mamun");
+const student2 = new student(22,"Apu");
+console.log(student1,student2);
